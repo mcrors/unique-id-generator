bd73926f5234ac1d817dd5b0e11a1a321025112d
rory.houlihan <rory.houlihan@vistaprint.com>
2020-11-08 10:25:52 +0100

    add DocStrings:

--- id_gen/gen_unique_id.py
+++ id_gen/gen_unique_id.py
@@ -1,9 +1,20 @@
 from typing import List
 import time
 import uuid
+import datetime
+
+today = datetime.datetime.now()
 
 
-def gen_unique_id(cnt: int) -> List:
+def gen_unique_id(cnt):
+    """Generate a list of unique IDs
+
+    Args:
+        cnt (int): The number of unique IDs you would like to be  returned
+
+    Returns:
+        List: A list of unique IDs
+    """
     sleep_for = ((cnt-1) // 10000) + 1
     time.sleep(sleep_for)
     return [uuid.uuid4() for _ in range(cnt)]
